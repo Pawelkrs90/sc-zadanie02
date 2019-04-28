aa9d5652d07316bb381081e85adb4c6e5cf869c1
Pawelkrs90 <Pawelkrs@hotmail.com>
2019-04-28 17:41:36 +0200

    Added lombok, created class with builder

---
+++ projectzad2/src/main/java/Main.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static void main( String[] args ){
+       Zad2Class zad2Class = Zad2Class.builder().id(0).Name("Test").value(10.0).build();
+    }
+}
---
+++ projectzad2/src/main/java/Zad2Class.java
@@ -0,0 +1,9 @@
+import lombok.Builder;
+
+@Builder
+public class Zad2Class {
+
+    private int id;
+    private String Name;
+    private double value;
+}
